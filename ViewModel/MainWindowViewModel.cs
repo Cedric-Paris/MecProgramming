d0a26a175b993c7808ebac7ff4fe5866fcbe618e
Cedric-Paris <cedric.paris@etu.utc.fr>
2018-10-20 18:12:36 -0400

    Autocomplete

--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using System.Windows.Input;
 using MecProgramming.Tools;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace MecProgramming.ViewModel
 {
@@ -11,10 +12,14 @@ namespace MecProgramming.ViewModel
 
         private SpeechSynthesizerManager synthesizerManager;
         private MailSender mailSender;
+        private Autocomplete autocomplete;
 
         private string displayedText = "";
         private bool isKeyBoardFeatureEnabled = true;
         private bool isCommonFeatureEnabled = false;
+        private string predictedWord1 = "";
+        private string predictedWord2 = "";
+        private string predictedWord3 = "";
 
         public string DisplayedText
         {
@@ -23,6 +28,7 @@ namespace MecProgramming.ViewModel
             {
                 displayedText = value;
                 OnPropertyChanged("DisplayedText");
+                UpdateAutoComplete();
             }
         }
 
@@ -49,6 +55,36 @@ namespace MecProgramming.ViewModel
             }
         }
 
+        public string PredictedWord1
+        {
+            get { return predictedWord1; }
+            set
+            {
+                predictedWord1 = value;
+                OnPropertyChanged("PredictedWord1");
+            }
+        }
+
+        public string PredictedWord2
+        {
+            get { return predictedWord2; }
+            set
+            {
+                predictedWord2 = value;
+                OnPropertyChanged("PredictedWord2");
+            }
+        }
+
+        public string PredictedWord3
+        {
+            get { return predictedWord3; }
+            set
+            {
+                predictedWord3 = value;
+                OnPropertyChanged("PredictedWord3");
+            }
+        }
+
         public Visibility KeyBoardVisibility
         {
             get { return IsKeyBoardFeatureEnabled ? Visibility.Visible : Visibility.Collapsed; }
@@ -69,11 +105,18 @@ namespace MecProgramming.ViewModel
 
         public ICommand OhYeahCommand { get { return new ButtonCommand(SpeakOhYeah); } }
 
+        public ICommand Pred1Command { get { return new ButtonCommand(() => AutocompliteDisplayedText(predictedWord1)); } }
+
+        public ICommand Pred2Command { get { return new ButtonCommand(() => AutocompliteDisplayedText(predictedWord2)); } }
+
+        public ICommand Pred3Command { get { return new ButtonCommand(() => AutocompliteDisplayedText(predictedWord3)); } }
+
         public MainWindowViewModel(SpeechSynthesizerManager synthesizerManager, MailSender mailSender)
         {
             this.synthesizerManager = synthesizerManager;
             this.mailSender = mailSender;
             CommonPhrasesViewModel = new CommonPhrasesViewModel(this.synthesizerManager);
+            autocomplete = new Autocomplete("proverbs.txt");
         }
 
         public void SpeakDisplayedText()
@@ -84,6 +127,51 @@ namespace MecProgramming.ViewModel
         public void SendDisplayedTextAsEmail()
         {
             mailSender.SendMail(DisplayedText);
+        }
+
+        public void UpdateAutoComplete()
+        {
+            var predicted = autocomplete.predict(DisplayedText);
+            if(predicted.Count > 0)
+                PredictedWord1 = predicted[0];
+            if (predicted.Count > 1)
+                PredictedWord2 = predicted[1];
+            if (predicted.Count > 2)
+                PredictedWord3 = predicted[2];
+        }
+
+        public void AutocompliteDisplayedText(string text)
+        {
+            if (DisplayedText.Length <= 0)
+            {
+                DisplayedText = text + " ";
+                return;
+            }
+            else
+            {
+                if (DisplayedText[DisplayedText.Length - 1] == ' ')
+                {
+                    DisplayedText += text + " ";
+                }
+                else
+                {
+                    bool flag = false;
+                    for(int i = DisplayedText.Length; i >= 0; i--)
+                    {
+                        if(DisplayedText[i] == ' ')
+                        {
+                            DisplayedText = string.Format("{0} {1} ", DisplayedText.Remove(i), text);
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if(!flag)
+                    {
+                        DisplayedText = string.Format("{0} ", text);
+                    }
+                }
+            }
+            DisplayedText = DisplayedText.ToUpper();
         }
 
         public void ToggleCommonFeature()
